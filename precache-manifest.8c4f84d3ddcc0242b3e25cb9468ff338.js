d722d6a95cbf8aa784d121ce1a2e724168eef7bf
sonam-ytrre <sonam@experienceflow.com>
2020-01-24 18:17:54 +0530

    Updates

---
+++ precache-manifest.8c4f84d3ddcc0242b3e25cb9468ff338.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfeed24e44a525e6cc20b9b9ee6ca0e5",
+    "url": "/marLabsAsgn/index.html"
+  },
+  {
+    "revision": "625c737e9e673affba3f",
+    "url": "/marLabsAsgn/static/css/main.3cada107.chunk.css"
+  },
+  {
+    "revision": "e245285d82b04b78163e",
+    "url": "/marLabsAsgn/static/js/2.4eac9d7b.chunk.js"
+  },
+  {
+    "revision": "70ab5fa1441caa1cae7d04d84d9f7613",
+    "url": "/marLabsAsgn/static/js/2.4eac9d7b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "625c737e9e673affba3f",
+    "url": "/marLabsAsgn/static/js/main.2e05137a.chunk.js"
+  },
+  {
+    "revision": "30f18dceec138c03f0d8",
+    "url": "/marLabsAsgn/static/js/runtime-main.c2a81a83.js"
+  }
+]);
